40d81e38a0169a4ace4fd1cd2aeba2459419e032
Ruion <liewyeat@gmail.com>
2019-10-28 23:25:46 +0800

    Change column name and sql table
    import the .sql to jumpshoot database

--- jumpshoot.sql
+++ jumpshoot.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2019 at 12:37 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Generation Time: Oct 28, 2019 at 04:23 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,8 +31,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `registration` (
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `phone` varchar(16) NOT NULL,
+  `contact` varchar(16) NOT NULL,
   `score` varchar(3) NOT NULL,
+  `game_score` text NOT NULL,
   `register_datetime` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,10 +41,17 @@ CREATE TABLE `registration` (
 -- Dumping data for table `registration`
 --
 
-INSERT INTO `registration` (`name`, `email`, `phone`, `score`, `register_datetime`) VALUES
-('a2', '0148929323', 'a2@gmail.com', '4', '2019-10-18 17:36:20'),
-('aw', '01494238298', 'aw@gmail.com', '2', '2019-10-18 17:12:38'),
-('a2', '0149999999', 'a3@gmail.com', '2', '2019-10-18 17:37:11');
+INSERT INTO `registration` (`name`, `email`, `contact`, `score`, `game_score`, `register_datetime`) VALUES
+('name1', 'email1', 'contact1', '', 'game_score1', 'register_datetime1'),
+('name10', 'email10', 'contact10', '', 'game_score10', 'register_datetime10'),
+('name2', 'email2', 'contact2', '', 'game_score2', 'register_datetime2'),
+('name3', 'email3', 'contact3', '', 'game_score3', 'register_datetime3'),
+('name4', 'email4', 'contact4', '', 'game_score4', 'register_datetime4'),
+('name5', 'email5', 'contact5', '', 'game_score5', 'register_datetime5'),
+('name6', 'email6', 'contact6', '', 'game_score6', 'register_datetime6'),
+('name7', 'email7', 'contact7', '', 'game_score7', 'register_datetime7'),
+('name8', 'email8', 'contact8', '', 'game_score8', 'register_datetime8'),
+('name9', 'email9', 'contact9', '', 'game_score9', 'register_datetime9');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +62,7 @@ INSERT INTO `registration` (`name`, `email`, `phone`, `score`, `register_datetim
 --
 ALTER TABLE `registration`
   ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `phone` (`phone`);
+  ADD UNIQUE KEY `phone` (`contact`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- jumpshoot/submit-data.php
+++ jumpshoot/submit-data.php
@@ -5,18 +5,19 @@ $json = array(
 		'result' => 'fail',
 );
 
-if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["phone"]) && isset($_POST["score"]) && isset($_POST["register_datetime"]))
+if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["contact"]) && isset($_POST["game_score"]) && isset($_POST["register_datetime"]))
 {
     require "connect.php";
 
     $name = mysqli_real_escape_string($con, $_POST["name"]);
     $email = mysqli_real_escape_string($con, $_POST["email"]);
-    $phone = mysqli_real_escape_string($con, $_POST["phone"]);
-    $score = mysqli_real_escape_string($con, $_POST["score"]);
+    $contact = mysqli_real_escape_string($con, $_POST["contact"]);
+   // $score = mysqli_real_escape_string($con, $_POST["score"]);
+    $game_score = mysqli_real_escape_string($con, $_POST["game_score"]);
     $register_datetime = mysqli_real_escape_string($con, $_POST["register_datetime"]);
 
-    $sql = mysqli_query($con, "INSERT INTO registration (name, phone, email, score, register_datetime) 
-    VALUES ('$name', '$phone', '$email', '$score', '$register_datetime')");
+    $sql = mysqli_query($con, "INSERT INTO registration (name, contact, email, game_score, register_datetime) 
+    VALUES ('$name', '$contact', '$email', '$game_score', '$register_datetime')");
 
     if ($sql)
     {
